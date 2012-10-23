f0a899a23d93dcb3a5281322b7d671d3b98b861f
Andrew Koester <andrew@neocodenetworks.com>
2012-10-23 02:54:43 -0700

    Adding more stack operation opcodes

--- MuftecBCL/FunctionClasses/StackOperations.cs
+++ MuftecBCL/FunctionClasses/StackOperations.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Muftec.Lib;
 
 namespace Muftec.BCL.FunctionClasses
@@ -32,6 +33,29 @@ namespace Muftec.BCL.FunctionClasses
 		{
 			data.RuntimeStack.Push(data.RuntimeStack.Peek());
 		}
+
+        /// <summary>
+        /// dupn ( ?n..?1 i -- ?n..?1 ?n..?1 )
+        /// Duplicates the top i stack items.
+        /// </summary>
+        /// <example>
+        /// 4 5 6 2 dupn ( leaves ) 4 5 6 5 6
+        /// </example>
+        /// <param name="data">Opcode reference data.</param>
+        [OpCode("dupn")]
+        public static void StackItemDupN(OpCodeData data)
+        {
+            var count = Shared.PopInt(data.RuntimeStack);
+            if (count == 0) return;
+            if (count < 0)
+                throw new MuftecInvalidStackItemTypeException(data.RuntimeStack);
+
+            var topItems = data.RuntimeStack.Take(count).Reverse().ToList();
+            foreach (var item in topItems)
+            {
+                data.RuntimeStack.Push(item);
+            }
+        }
 
 		/// <summary>
 		/// pop ( x..X -- )
@@ -84,5 +108,198 @@ namespace Muftec.BCL.FunctionClasses
 			data.RuntimeStack.Push(item2);
 			data.RuntimeStack.Push(item1);
 		}
+
+        /// <summary>
+        /// over ( x y -- x y x )
+        /// Duplicates the second-to-top thing on the stack. This is the same as 2 pick.
+        /// </summary>
+        /// <example>
+        /// 1 2 over ( returns ) 1 2 1
+        /// </example>
+        /// <param name="data">Opcode reference data.</param>
+        [OpCode("over")]
+        public static void StackItemOver(OpCodeData data)
+        {
+            data.RuntimeStack.Push(new MuftecStackItem(2));
+            StackItemPick(data);
+        }
+
+        /// <summary>
+        /// rot ( x y z -- y z x )
+        /// Rotates the top three things on the stack. This is equivalent to 3 rotate.
+        /// </summary>
+        /// <example>
+        /// 1 2 3 rot ( returns ) 2 3 1
+        /// </example>
+        /// <param name="data">Opcode reference data.</param>
+        [OpCode("rot")]
+        public static void StackItemRot(OpCodeData data)
+        {
+            data.RuntimeStack.Push(new MuftecStackItem(3));
+            StackItemRotate(data);
+        }
+
+        /// <summary>
+        /// rotate ( ni ... n1 i -- n(i-1) ... n1 ni )
+        /// Rotates the top i things on the stack. Using a negative rotational value rotates backwards.
+        /// </summary>
+        /// <example>
+        /// 1 2 3 4 4 rotate ( returns ) 2 3 4 1
+        /// </example>
+        /// <param name="data">Opcode reference data.</param>
+        [OpCode("rotate")]
+        public static void StackItemRotate(OpCodeData data)
+        {
+            var position = Shared.PopInt(data.RuntimeStack);
+            if (position == 0) return; // nop
+
+            var popped = new List<MuftecStackItem>();
+
+            // Get all items in stack
+            for (var i = 0; i < position; i++)
+            {
+                var val = data.RuntimeStack.Pop();
+                popped.Add(val);
+            }
+
+            // Re-add in original order
+            var remaining = popped.Take(popped.Count - 1).Reverse();
+
+            foreach (var item in remaining)
+            {
+                data.RuntimeStack.Push(item);
+            }
+
+            // Add rotated item last
+            data.RuntimeStack.Push(popped.Last());
+        }
+
+        /// <summary>
+        /// pick ( ni ... n1 i -- ni ... n1 ni )
+        /// Takes the i'th thing from the top of the stack and pushes it on the top.
+        /// 1 pick is equivalent to dup, and 2 pick is equivalent to over.
+        /// </summary>
+        /// <example>
+        /// 1 2 3 3 pick ( returns ) 1 2 3 1
+        /// </example>
+        /// <param name="data">Opcode reference data.</param>
+        [OpCode("pick")]
+        public static void StackItemPick(OpCodeData data)
+        {
+            var position = Shared.PopInt(data.RuntimeStack);
+            if (position == 0) return;
+            if (position < 0)
+                throw new MuftecInvalidStackItemTypeException(data.RuntimeStack);
+
+            var item = data.RuntimeStack.Skip(position - 1).Take(1).Single();
+            data.RuntimeStack.Push(item);
+        }
+
+        /// <summary>
+        /// put ( nx...n1 ni i -- nx...ni...n1 )
+        /// Replaces the i'th item from the top of the stack with the value of ni.
+        /// The command sequence '1 put' is equivalent to 'swap pop'.
+        /// </summary>
+        /// <example>
+        /// 1 2 3 2 put ( returns ) 1 3 3
+        /// </example>
+        /// <param name="data">Opcode reference data.</param>
+        [OpCode("put")]
+        public static void StackItemPut(OpCodeData data)
+        {
+            var position = Shared.PopInt(data.RuntimeStack);
+            if (position <= 0)
+                throw new MuftecInvalidStackItemTypeException(data.RuntimeStack);
+
+            var putItem = Shared.Pop(data.RuntimeStack);
+
+            // Get all items in stack
+            var popped = new List<MuftecStackItem>();
+            for (var i = 0; i < position - 1; i++)
+            {
+                var poppedItem = data.RuntimeStack.Pop();
+                popped.Add(poppedItem);
+            }
+
+            // Remove last item
+            data.RuntimeStack.Pop();
+
+            // Put moved item in its place
+            data.RuntimeStack.Push(putItem);
+
+            // Re-add rest in original order
+            popped.Reverse();
+            foreach (var item in popped)
+            {
+                data.RuntimeStack.Push(item);
+            }
+        }
+
+        /// <summary>
+        /// reverse ( ?n..?1 i -- ?1..?n )
+        /// Reverses the order of the top i items on the stack.
+        /// </summary>
+        /// <example>
+        /// 1 2 3 4 3 reverse ( returns ) 1 4 3 2
+        /// </example>
+        /// <param name="data">Opcode reference data.</param>
+        [OpCode("reverse")]
+        public static void StackItemReverse(OpCodeData data)
+        {
+            var count = Shared.PopInt(data.RuntimeStack);
+            if (count == 0) return;
+            if (count < 0)
+                throw new MuftecInvalidStackItemTypeException(data.RuntimeStack);
+
+            // Take n items
+            var popped = new List<MuftecStackItem>();
+
+            for (var i = 0; i < count; i++)
+            {
+                var item = data.RuntimeStack.Pop();
+                popped.Add(item);
+            }
+
+            // Push in reverse order
+            foreach (var item in popped)
+            {
+                data.RuntimeStack.Push(item);
+            }
+        }
+
+        /// <summary>
+        /// lreverse ( ?n..?1 i -- ?1..?n )
+        /// Reverses the order of the top i stack items, leaving i.
+        /// </summary>
+        /// <example>
+        /// 1 2 3 4 3 reverse ( returns ) 1 4 3 2 3
+        /// </example>
+        /// <param name="data">Opcode reference data.</param>
+        [OpCode("lreverse")]
+        public static void StackItemLReverse(OpCodeData data)
+        {
+            var count = Shared.PopInt(data.RuntimeStack);
+            if (count == 0) return;
+            if (count < 0)
+                throw new MuftecInvalidStackItemTypeException(data.RuntimeStack);
+
+            // Take n items
+            var popped = new List<MuftecStackItem>();
+
+            for (var i = 0; i < count; i++)
+            {
+                var item = data.RuntimeStack.Pop();
+                popped.Add(item);
+            }
+
+            // Push in reverse order
+            foreach (var item in popped)
+            {
+                data.RuntimeStack.Push(item);
+            }
+
+            // Re-add count
+            data.RuntimeStack.Push(new MuftecStackItem(count));
+        }
 	}
 }
--- MuftecTest/StackOperationsTest.cs
+++ MuftecTest/StackOperationsTest.cs
@@ -60,8 +60,109 @@ namespace Muftec.Test
 		//
 		#endregion
 
+        /// <summary>
+        ///A test for StackDepth
+        ///</summary>
+        [TestMethod]
+        public void StackDepthTest()
+        {
+            var runtimeStack = new Stack<MuftecStackItem>();
+            var data = new OpCodeData(runtimeStack);
+            runtimeStack.Push(new MuftecStackItem("Alfredo"));
+            runtimeStack.Push(new MuftecStackItem(1.99));
+            StackOperations.StackDepth(data);
 
-		/// <summary>
+            var runtimeStackExpected = new Stack<MuftecStackItem>();
+            runtimeStackExpected.Push(new MuftecStackItem("Alfredo"));
+            runtimeStackExpected.Push(new MuftecStackItem(1.99));
+            runtimeStackExpected.Push(new MuftecStackItem(2));
+
+            TestShared.CompareStacks(runtimeStack, runtimeStackExpected);
+        }
+
+        /// <summary>
+        ///A test for StackItemDup
+        ///</summary>
+        [TestMethod]
+        public void StackItemDupTest()
+        {
+            var runtimeStack = new Stack<MuftecStackItem>();
+            var data = new OpCodeData(runtimeStack);
+            runtimeStack.Push(new MuftecStackItem("Alfredo"));
+            runtimeStack.Push(new MuftecStackItem(1.99));
+            StackOperations.StackItemDup(data);
+
+            var runtimeStackExpected = new Stack<MuftecStackItem>();
+            runtimeStackExpected.Push(new MuftecStackItem("Alfredo"));
+            runtimeStackExpected.Push(new MuftecStackItem(1.99));
+            runtimeStackExpected.Push(new MuftecStackItem(1.99));
+
+            TestShared.CompareStacks(runtimeStack, runtimeStackExpected);
+        }
+
+        /// <summary>
+        /// A test for StackItemDupN
+        /// </summary>
+        [TestMethod]
+        public void StackItemDupNTest()
+        {
+            var runtimeStack = new Stack<MuftecStackItem>();
+            var data = new OpCodeData(runtimeStack);
+            runtimeStack.Push(new MuftecStackItem(4));
+            runtimeStack.Push(new MuftecStackItem(1.99));
+            runtimeStack.Push(new MuftecStackItem("Alfredo"));
+            runtimeStack.Push(new MuftecStackItem(2));
+            StackOperations.StackItemDupN(data);
+
+            var runtimeStackExpected = new Stack<MuftecStackItem>();
+            runtimeStackExpected.Push(new MuftecStackItem(4));
+            runtimeStackExpected.Push(new MuftecStackItem(1.99));
+            runtimeStackExpected.Push(new MuftecStackItem("Alfredo"));
+            runtimeStackExpected.Push(new MuftecStackItem(1.99));
+            runtimeStackExpected.Push(new MuftecStackItem("Alfredo"));
+
+            TestShared.CompareStacks(runtimeStack, runtimeStackExpected);
+        }
+
+        /// <summary>
+        ///A test for StackItemPop
+        ///</summary>
+        [TestMethod]
+        public void StackItemPopTest()
+        {
+            var runtimeStack = new Stack<MuftecStackItem>();
+            var data = new OpCodeData(runtimeStack);
+            runtimeStack.Push(new MuftecStackItem("Alfredo"));
+            runtimeStack.Push(new MuftecStackItem(1.99));
+            StackOperations.StackItemPop(data);
+
+            var runtimeStackExpected = new Stack<MuftecStackItem>();
+            runtimeStackExpected.Push(new MuftecStackItem("Alfredo"));
+
+            TestShared.CompareStacks(runtimeStack, runtimeStackExpected);
+        }
+
+        /// <summary>
+        ///A test for StackItemPopN
+        ///</summary>
+        [TestMethod]
+        public void StackItemPopNTest()
+        {
+            var runtimeStack = new Stack<MuftecStackItem>();
+            var data = new OpCodeData(runtimeStack);
+            runtimeStack.Push(new MuftecStackItem("Alfredo"));
+            runtimeStack.Push(new MuftecStackItem(1.99));
+            runtimeStack.Push(new MuftecStackItem("Class action"));
+            runtimeStack.Push(new MuftecStackItem(2));
+            StackOperations.StackItemPopN(data);
+
+            var runtimeStackExpected = new Stack<MuftecStackItem>();
+            runtimeStackExpected.Push(new MuftecStackItem("Alfredo"));
+
+            TestShared.CompareStacks(runtimeStack, runtimeStackExpected);
+        }
+
+        /// <summary>
 		///A test for StackItemSwap
 		///</summary>
 		[TestMethod]
@@ -80,82 +181,160 @@ namespace Muftec.Test
             TestShared.CompareStacks(runtimeStack, runtimeStackExpected);
 		}
 
-		/// <summary>
-		///A test for StackItemPopN
-		///</summary>
-		[TestMethod]
-		public void StackItemPopNTest()
-		{
-		    var runtimeStack = new Stack<MuftecStackItem>();
-		    var data = new OpCodeData(runtimeStack);
-		    runtimeStack.Push(new MuftecStackItem("Alfredo"));
-			runtimeStack.Push(new MuftecStackItem(1.99));
-			runtimeStack.Push(new MuftecStackItem("Class action"));
-			runtimeStack.Push(new MuftecStackItem(2));
-			StackOperations.StackItemPopN(data);
+        /// <summary>
+        /// A test for StackItemOver
+        /// </summary>
+        [TestMethod]
+        public void StackItemOverTest()
+        {
+            var runtimeStack = new Stack<MuftecStackItem>();
+            var data = new OpCodeData(runtimeStack);
+            runtimeStack.Push(new MuftecStackItem("Alfredo"));
+            runtimeStack.Push(new MuftecStackItem(1.99));
+            StackOperations.StackItemOver(data);
 
-			var runtimeStackExpected = new Stack<MuftecStackItem>();
-			runtimeStackExpected.Push(new MuftecStackItem("Alfredo"));
-
-            TestShared.CompareStacks(runtimeStack, runtimeStackExpected);
-		}
-
-		/// <summary>
-		///A test for StackItemPop
-		///</summary>
-		[TestMethod]
-		public void StackItemPopTest()
-		{
-		    var runtimeStack = new Stack<MuftecStackItem>();
-		    var data = new OpCodeData(runtimeStack);
-		    runtimeStack.Push(new MuftecStackItem("Alfredo"));
-			runtimeStack.Push(new MuftecStackItem(1.99));
-			StackOperations.StackItemPop(data);
-
-			var runtimeStackExpected = new Stack<MuftecStackItem>();
-			runtimeStackExpected.Push(new MuftecStackItem("Alfredo"));
-
-            TestShared.CompareStacks(runtimeStack, runtimeStackExpected);
-		}
-
-		/// <summary>
-		///A test for StackItemDup
-		///</summary>
-		[TestMethod]
-		public void StackItemDupTest()
-		{
-		    var runtimeStack = new Stack<MuftecStackItem>();
-		    var data = new OpCodeData(runtimeStack);
-		    runtimeStack.Push(new MuftecStackItem("Alfredo"));
-			runtimeStack.Push(new MuftecStackItem(1.99));
-			StackOperations.StackItemDup(data);
-
-			var runtimeStackExpected = new Stack<MuftecStackItem>();
-			runtimeStackExpected.Push(new MuftecStackItem("Alfredo"));
-			runtimeStackExpected.Push(new MuftecStackItem(1.99));
-			runtimeStackExpected.Push(new MuftecStackItem(1.99));
-
-            TestShared.CompareStacks(runtimeStack, runtimeStackExpected);
-		}
-
-		/// <summary>
-		///A test for StackDepth
-		///</summary>
-		[TestMethod]
-		public void StackDepthTest()
-		{
-		    var runtimeStack = new Stack<MuftecStackItem>();
-		    var data = new OpCodeData(runtimeStack);
-		    runtimeStack.Push(new MuftecStackItem("Alfredo"));
-			runtimeStack.Push(new MuftecStackItem(1.99));
-			StackOperations.StackDepth(data);
-
-			var runtimeStackExpected = new Stack<MuftecStackItem>();
+            var runtimeStackExpected = new Stack<MuftecStackItem>();
             runtimeStackExpected.Push(new MuftecStackItem("Alfredo"));
             runtimeStackExpected.Push(new MuftecStackItem(1.99));
-			runtimeStackExpected.Push(new MuftecStackItem(2));
+            runtimeStackExpected.Push(new MuftecStackItem("Alfredo"));
 
             TestShared.CompareStacks(runtimeStack, runtimeStackExpected);
-		}
+        }
+
+        /// <summary>
+        ///A test for StackItemRot
+        ///</summary>
+        [TestMethod]
+        public void StackItemRotTest()
+        {
+            var runtimeStack = new Stack<MuftecStackItem>();
+            var data = new OpCodeData(runtimeStack);
+            runtimeStack.Push(new MuftecStackItem("Alfredo"));
+            runtimeStack.Push(new MuftecStackItem(1.99));
+            runtimeStack.Push(new MuftecStackItem(2));
+            StackOperations.StackItemRot(data);
+
+            var runtimeStackExpected = new Stack<MuftecStackItem>();
+            runtimeStackExpected.Push(new MuftecStackItem(1.99));
+            runtimeStackExpected.Push(new MuftecStackItem(2));
+            runtimeStackExpected.Push(new MuftecStackItem("Alfredo"));
+
+            TestShared.CompareStacks(runtimeStack, runtimeStackExpected);
+        }
+
+        /// <summary>
+        ///A test for StackItemRotate
+        ///</summary>
+        [TestMethod]
+        public void StackItemRotateTest()
+        {
+            var runtimeStack = new Stack<MuftecStackItem>();
+            var data = new OpCodeData(runtimeStack);
+            runtimeStack.Push(new MuftecStackItem("Alfredo"));
+            runtimeStack.Push(new MuftecStackItem(1.99));
+            runtimeStack.Push(new MuftecStackItem(2));
+            runtimeStack.Push(new MuftecStackItem("pizza"));
+            runtimeStack.Push(new MuftecStackItem(4));
+            StackOperations.StackItemRotate(data);
+
+            var runtimeStackExpected = new Stack<MuftecStackItem>();
+            runtimeStackExpected.Push(new MuftecStackItem(1.99));
+            runtimeStackExpected.Push(new MuftecStackItem(2));
+            runtimeStackExpected.Push(new MuftecStackItem("pizza"));
+            runtimeStackExpected.Push(new MuftecStackItem("Alfredo"));
+
+            TestShared.CompareStacks(runtimeStack, runtimeStackExpected);
+        }
+
+        /// <summary>
+        /// A test for StackItemPick
+        /// </summary>
+        [TestMethod]
+        public void StackItemPickTest()
+        {
+            var runtimeStack = new Stack<MuftecStackItem>();
+            var data = new OpCodeData(runtimeStack);
+            runtimeStack.Push(new MuftecStackItem("Alfredo"));
+            runtimeStack.Push(new MuftecStackItem(1.99));
+            runtimeStack.Push(new MuftecStackItem(5));
+            runtimeStack.Push(new MuftecStackItem(3));
+            StackOperations.StackItemPick(data);
+
+            var runtimeStackExpected = new Stack<MuftecStackItem>();
+            runtimeStackExpected.Push(new MuftecStackItem("Alfredo"));
+            runtimeStackExpected.Push(new MuftecStackItem(1.99));
+            runtimeStackExpected.Push(new MuftecStackItem(5));
+            runtimeStackExpected.Push(new MuftecStackItem("Alfredo"));
+
+            TestShared.CompareStacks(runtimeStack, runtimeStackExpected);
+        }
+
+        /// <summary>
+        /// A test for StackItemPut
+        /// </summary>
+        [TestMethod]
+        public void StackItemPutTest()
+        {
+            var runtimeStack = new Stack<MuftecStackItem>();
+            var data = new OpCodeData(runtimeStack);
+            runtimeStack.Push(new MuftecStackItem("Alfredo"));
+            runtimeStack.Push(new MuftecStackItem(1.99));
+            runtimeStack.Push(new MuftecStackItem(5));
+            runtimeStack.Push(new MuftecStackItem(7.7));
+            runtimeStack.Push(new MuftecStackItem(2));
+            StackOperations.StackItemPut(data);
+
+            var runtimeStackExpected = new Stack<MuftecStackItem>();
+            runtimeStackExpected.Push(new MuftecStackItem("Alfredo"));
+            runtimeStackExpected.Push(new MuftecStackItem(7.7));
+            runtimeStackExpected.Push(new MuftecStackItem(5));
+
+            TestShared.CompareStacks(runtimeStack, runtimeStackExpected);
+        }
+
+        /// <summary>
+        /// A test for StackItemReverse
+        /// </summary>
+        [TestMethod]
+        public void StackItemReverseTest()
+        {
+            var runtimeStack = new Stack<MuftecStackItem>();
+            var data = new OpCodeData(runtimeStack);
+            runtimeStack.Push(new MuftecStackItem("Alfredo"));
+            runtimeStack.Push(new MuftecStackItem(1.99));
+            runtimeStack.Push(new MuftecStackItem(5));
+            runtimeStack.Push(new MuftecStackItem(2));
+            StackOperations.StackItemReverse(data);
+
+            var runtimeStackExpected = new Stack<MuftecStackItem>();
+            runtimeStackExpected.Push(new MuftecStackItem("Alfredo"));
+            runtimeStackExpected.Push(new MuftecStackItem(5));
+            runtimeStackExpected.Push(new MuftecStackItem(1.99));
+
+            TestShared.CompareStacks(runtimeStack, runtimeStackExpected);
+        }
+
+        /// <summary>
+        /// A test for StackItemLReverse
+        /// </summary>
+        [TestMethod]
+        public void StackItemLReverseTest()
+        {
+            var runtimeStack = new Stack<MuftecStackItem>();
+            var data = new OpCodeData(runtimeStack);
+            runtimeStack.Push(new MuftecStackItem("Alfredo"));
+            runtimeStack.Push(new MuftecStackItem(1.99));
+            runtimeStack.Push(new MuftecStackItem(5));
+            runtimeStack.Push(new MuftecStackItem(2));
+            StackOperations.StackItemLReverse(data);
+
+            var runtimeStackExpected = new Stack<MuftecStackItem>();
+            runtimeStackExpected.Push(new MuftecStackItem("Alfredo"));
+            runtimeStackExpected.Push(new MuftecStackItem(5));
+            runtimeStackExpected.Push(new MuftecStackItem(1.99));
+            runtimeStackExpected.Push(new MuftecStackItem(2));
+
+            TestShared.CompareStacks(runtimeStack, runtimeStackExpected);
+        }
 	}
 }
